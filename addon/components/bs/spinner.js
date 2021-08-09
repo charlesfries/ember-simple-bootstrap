d596ccc102fed939437d1c8151a6d6d5c6c21109
Charles Fries <charliefries@icloud.com>
2021-08-09 12:19:37 -0700

    Generate progress components

--- addon/components/bs/spinner.js
+++ addon/components/bs/spinner.js
@@ -1,3 +1,10 @@
 import BsBaseComponent from './base';
 
+/**
+ * ARGS
+ * centered?: boolean
+ * large?: boolean
+ * color?: 'primary'|'secondary'|... = 'secondary'
+ */
+
 export default class BsSpinnerComponent extends BsBaseComponent {}
--- addon/components/bs/toolbar.js
+++ addon/components/bs/toolbar.js
@@ -1,3 +1,8 @@
 import BsBaseComponent from './base';
 
+/**
+ * ARGS
+ * size?: 'sm'|'lg'
+ */
+
 export default class BsToolbarComponent extends BsBaseComponent {}
