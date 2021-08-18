db6bd5b0bc549a9a402a9cda9935756488898ae4
Charles Fries <charliefries@icloud.com>
2021-08-17 17:46:13 -0700

    Update tooltip modifier

--- addon/modifiers/tooltip.ts
+++ addon/modifiers/tooltip.ts
@@ -1,21 +1,31 @@
 import { modifier } from 'ember-modifier';
 /* global bootstrap */
 
-export default modifier(function tooltip(element: Element, [title, placement = 'top']) {
-	element.setAttribute('data-bs-toggle', 'tooltip');
-	element.setAttribute('data-bs-placement', placement as string);
-	element.setAttribute('title', title as string);
+interface Args {
+	animation?: boolean;
+	delay?: number | Record<string, unknown>;
+	html?: boolean;
+	placement?: 'auto' | 'top' | 'bottom' | 'left' | 'right';
+	selector?: string | boolean;
+	template?: string;
+	trigger?: string;
+	fallbackPlacements?: string[];
+	boundary?: string | Element;
+	offset?: number[] | string | Function;
+}
+
+export default modifier(function tooltip(element: Element, [title], args: Args) {
+	const { animation, delay, placement } = args;
 
 	// @ts-ignore
 	const tooltip = new bootstrap.Tooltip(element, {
-		animation: false,
+		title,
+		animation: animation ?? true,
+		delay: delay ?? 0,
+		placement: placement ?? 'top',
 	});
 
 	return () => {
-		element.removeAttribute('data-bs-toggle');
-		element.removeAttribute('data-bs-placement');
-		element.removeAttribute('title');
-
 		tooltip.dispose();
 	};
 });
