aba2deddcbe7b077808b2d63391871e8d89096ce
Charles Fries <charliefries@icloud.com>
2022-02-16 09:07:14 -0800

    Lint

--- tests/integration/components/bs/pagination-test.ts
+++ tests/integration/components/bs/pagination-test.ts
@@ -1,20 +1,24 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click } from '@ember/test-helpers';
+import { render, TestContext, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
+interface Context extends TestContext {
+  curPage: number;
+}
 
 module('Integration | Component | bs/pagination', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('it renders', async function (this: Context, assert) {
     this.set('curPage', 0);
     this.set('perPage', 5);
     this.set('totalItems', 21);
     this.set('next', () => {
-      this.set('curPage', this.get('curPage') + 1);
+      this.set('curPage', this.curPage + 1);
     });
     this.set('previous', () => {
-      this.set('curPage', this.get('curPage') - 1);
+      this.set('curPage', this.curPage - 1);
     });
     this.set('select', (page: number) => {
       this.set('curPage', page);
@@ -34,18 +38,18 @@ module('Integration | Component | bs/pagination', function (hooks) {
     assert.dom('nav').exists();
     assert.dom('nav .pagination').exists();
     assert.dom('nav .pagination .page-item').exists({ count: 7 });
-    assert.dom(this.element).hasText('Previous 1 2 3 4 5 Next');
-    console.log(document.querySelector('[data-test-previous]'))
+    assert.dom(this.element as Element).hasText('Previous 1 2 3 4 5 Next');
+    console.log(document.querySelector('[data-test-previous]'));
     assert.dom('.page-item:nth-child(1)').hasClass('disabled');
 
     await click('[data-test-next]');
-    assert.strictEqual(this.get('curPage'), 1);
+    assert.strictEqual(this.curPage, 1);
 
     await click('[data-test-previous]');
-    assert.strictEqual(this.get('curPage'), 0);
+    assert.strictEqual(this.curPage, 0);
 
     await click('.page-item:nth-child(6) .page-link');
-    assert.strictEqual(this.get('curPage'), 4);
+    assert.strictEqual(this.curPage, 4);
     assert.dom('.page-item:nth-child(7)').hasClass('disabled');
   });
 });
