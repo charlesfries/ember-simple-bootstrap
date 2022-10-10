bc35f954e403c787b178cccf6dfece80f53c0849
Charles Fries <charliefries@icloud.com>
2022-10-10 12:21:36 -0700

    UELS bug

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,4 +3,13 @@
 module.exports = {
   plugins: ['ember-template-lint-plugin-prettier'],
   extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  overrides: [
+    // fixes Unstable Ember Language Server prettier bug
+    {
+      files: ['**/integration/**/*-test.{js,ts}'],
+      rules: {
+        prettier: false,
+      },
+    },
+  ],
 };
