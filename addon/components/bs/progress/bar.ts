9586bd2839447a60618407cb837972c45aa0eca8
Charles Fries <charliefries@icloud.com>
2022-10-10 12:26:02 -0700

    Rm `html-safe` usage

--- addon/components/bs/progress/bar.ts
+++ addon/components/bs/progress/bar.ts
@@ -1,6 +1,9 @@
 import BsBaseComponent from '../base';
 import { assert } from '@ember/debug';
 import { typeOf } from '@ember/utils';
+import { htmlSafe } from '@ember/template';
+
+import type { SafeString } from '@ember/template/-private/handlebars';
 
 export interface BsProgressBarComponentArgs {
   value: number;
@@ -19,5 +22,9 @@ export default class BsProgressBarComponent<
       '<Bs::Progress::Bar />: Must pass a value number',
       typeOf(this.args.value) === 'number'
     );
+  }
+
+  get style(): SafeString {
+    return htmlSafe(`width: ${this.args.value}%;`);
   }
 }
